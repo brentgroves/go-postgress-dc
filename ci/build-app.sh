e3c17a42a5df58d738496a0f08b7c409eb6ca19d
brentgroves <brent.groves@gmail.com>
2023-06-14 19:10:01 -0400

    Updated source code

---
+++ ci/build-app.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# ci/build-app.sh 
+set -e
+cd "$(dirname $0)"
+
+build_date="$(date +%s)"
+
+# Create a label for use during cleanup since the devcontainer CLI does 
+# not have a "remove" or "down" command yet (though this is planned).
+id_label="ci-container=${build_date}"
+
+# Run build
+devcontainer up --id-label ${id_label} --workspace-folder ../..
+set +e
+devcontainer exec --id-label ${id_label} --workspace-folder ../.. app-build/app-build.sh
+build_exit_code=$?
+set -e
+
+# Clean up. 
+echo "\nCleaning up..."
+docker rm -f $(docker ps -aq --filter label=${id_label})
+
+exit ${build_exit_code}
